7b2bd3a5c0084fc6c04090dc7974a5b80604c9da
KirTim <kirtim1986@gmail.com>
2021-03-05 18:42:23 +0300

    addet verification script to registration-form

--- script.js
+++ script.js
@@ -29,10 +29,11 @@ console.log(`Server started on port ${port}`);
 
 
 const server = http.createServer(function (request, response) {
-    console.log(`Запрошенный адрес: ${request.url}`)
+  console.log(`Запрошенный адрес: ${request.url}`)
+  try {
     let filePath = './public' + request.url
-    if(fs.statSync(filePath).isDirectory()){
-        filePath += '/index.html';
+    if (fs.statSync(filePath).isDirectory()) {
+      filePath += '/index.html';
     }
     fs.access(filePath, fs.constants.R_OK, (err) => {
       if (err) {
@@ -42,11 +43,12 @@ const server = http.createServer(function (request, response) {
         fs.createReadStream(filePath).pipe(response)
       }
     })
-  })
-  
+  } catch (err) {
+    console.log('Ошибка сервера')
+  }
+})
+
 const port = process.env.PORT || 8181
 server.listen(port);
 
 console.log(`Server started on port ${port}`);
-
-
